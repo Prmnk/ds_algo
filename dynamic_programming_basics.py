34d2ab20e73a984ddbf49e7f3c05c67f7184016c
Prmnk <pramanikota@gmail.com>
2021-06-27 00:49:16 +0530

    dp

--- dynamic_programming_basics.py
+++ dynamic_programming_basics.py
@@ -10,7 +10,7 @@ def fib(n):
 print(fib(5))
 
 
-# memoized solution
+# memoized solution -- recursive plus save result to refer to avoid re-calculation
 # save results of fib(n) at nth place in an array
 
 def fib_memo(n, memo):
@@ -27,7 +27,7 @@ def fib_memo(n, memo):
 print(fib_memo(5, [None] * 6))
 
 
-# botton up
+# botton up -- 
 
 def fib_bottomup(n):
     if n==1 or n==2:
@@ -44,3 +44,40 @@ def fib_bottomup(n):
 print(fib_bottomup(5))
 
     
+# bottom up approach, loop + calculate and store data in an array
+
+def factorial_dp(n):
+    f = [0]*(n+1)
+    f[0]  = 1
+    for i in range(1,n+1):
+        f[i] = f[i-1] * i
+    return f[n]
+
+#print(factorial_dp(5))
+
+# https://www.geeksforgeeks.org/dynamic-programming-high-effort-vs-low-effort-tasks-problem/
+
+def max_tasks(high, low, n):
+
+    if n <= 0:
+        return 0
+
+    return max(high[n-1]+max_tasks(high,low,n-2) , low[n-1]+ max_tasks(high, low, n-1))
+
+def max_task_dp(high, low, n):
+    task_dp = [0] *(n+1)
+    task_dp[0] = 0
+    task_dp[1] = high[0]
+
+    for i in range(2,n+1):
+        task_dp[i] = max( high[i-1] +task_dp[i-2], low[i-1]+ task_dp[i-1] )
+
+    return task_dp[n]
+
+n = 5
+high = [3, 6, 8, 7, 6]
+low = [1, 5, 4, 5, 3]
+print(max_tasks(high, low, n))
+print(max_task_dp(high, low, n))
+
+
